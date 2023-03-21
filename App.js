6e9b34490786a1d4f37dfead37e45e26a112353c
Aditya Vandan Sharma <adityavandansharma@Adityas-MacBook-Pro.local>
2023-03-21 14:06:52 +0530

    feat: webapp usage

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {getSum} from './tippling-webapp/src/utils';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! {getSum(1,3)}</Text>
       <StatusBar style="auto" />
     </View>
   );
